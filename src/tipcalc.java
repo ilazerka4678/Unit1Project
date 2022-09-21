66b10fcda3458612b3d6bd6da820500a8e171fa8
BT_4N2_05 <ilazerka4678@bths.edu>
2022-09-21 11:42:13 -0400

    Added scanner object

--- src/tipcalc.java
+++ src/tipcalc.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class tipcalc {
     public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
     //User input here
     System.out.print("Please input your total bill: ");
     int bill = s.nextInt();
@@ -13,13 +14,13 @@ public class tipcalc {
     System.out.println();
     System.out.println();
     //Calculations
-    double tipAmount = (bill/100)*tipPercent;
+    double tipAmount = (bill/100.0)*tipPercent;
     double tipPerPerson = tipAmount/personCount;
     double billTotal = bill + tipAmount;
     double billSplit = billTotal/personCount;
     //Display
     System.out.println("Total bill: " + billTotal);
-    
+
 
     }
 }
